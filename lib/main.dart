7962b2989591fbd27c73f9d8c68d2ece3e130e3c
Justin McCandless <justinjmccandless@gmail.com>
2023-10-18 15:16:21 -0700

    GitHub pages doesnt support non-hash page nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'router.dart';
 
 void main() {
-  usePathUrlStrategy();
   runApp(
     const ProviderScope(
       child: ReleasesApp(),
